23885510abb09af795d4c1f2b9427ab54ba80749
SakshiCs124 <acharyasakshi399@gmail.com>
2025-05-19 11:40:18 +0530

    Add files via upload

---
+++ DisplayElementsWithPositions.java
@@ -0,0 +1,21 @@
+package github;
+
+import java.util.LinkedList;
+
+public class DisplayElementsWithPositions {
+    public static void main(String[] args) {
+        // Create a LinkedList of colors
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        // Display elements with their positions using get(index)
+        System.out.println("Elements and their positions in the LinkedList:");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println("Position " + i + ": " + colors.get(i));
+        }
+    }
+}
