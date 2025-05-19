7a111672dd1044f80971e66d00636dd98f1de4d5
SakshiCs124 <acharyasakshi399@gmail.com>
2025-05-19 11:05:19 +0530

    Add files via upload

---
+++ IterateFromSecond.java
@@ -0,0 +1,27 @@
+package github;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class IterateFromSecond {
+    public static void main(String[] args) {
+        // Creating a LinkedList of colors
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        // Display original list
+        System.out.println("Original LinkedList: " + colors);
+
+        // Creating ListIterator starting from index 1 (2nd position)
+        ListIterator<String> iterator = colors.listIterator(1);
+
+        System.out.println("Iterating from 2nd element:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
