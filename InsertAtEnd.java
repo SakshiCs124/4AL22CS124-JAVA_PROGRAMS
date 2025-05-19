6a907f50997d07c27cbf79c8d4e6654c8e6afeb0
SakshiCs124 <acharyasakshi399@gmail.com>
2025-05-19 11:32:30 +0530

    Add files via upload

---
+++ InsertAtEnd.java
@@ -0,0 +1,22 @@
+package github;
+
+import java.util.LinkedList;
+
+public class InsertAtEnd {
+    public static void main(String[] args) {
+        // Creating a LinkedList of colors
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+
+        // Display original list
+        System.out.println("Original LinkedList: " + colors);
+
+        // Insert "Pink" at the end using offerLast()
+        colors.offerLast("Pink");
+
+        // Display updated list
+        System.out.println("Updated LinkedList after inserting 'Pink' at the end: " + colors);
+    }
+}
