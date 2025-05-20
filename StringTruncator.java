3ab71fc30e4f0610dd3e6cc304fa8e2c8d59f50b
SakshiCs124 <acharyasakshi399@gmail.com>
2025-05-20 18:57:54 +0530

    Add files via upload

---
+++ StringTruncator.java
@@ -0,0 +1,36 @@
+package github;
+
+import java.util.Scanner;
+
+public class StringTruncator {
+
+    // User-defined function to truncate string and add ellipsis
+    public static String truncate(String input, int length) {
+        if (input.length() <= length) {
+            return input; // Return the string as is if it's shorter or equal to the desired length
+        } else {
+            return input.substring(0, length) + "..."; // Truncate and add ellipsis
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get input from the user
+        System.out.print("Enter a string: ");
+        String userInput = scanner.nextLine();
+
+        // Get the desired length from the user
+        System.out.print("Enter the length to truncate to: ");
+        int length = scanner.nextInt();
+
+        // Call the truncate function
+        String result = truncate(userInput, length);
+
+        // Display the result
+        System.out.println("Truncated string: " + result);
+
+        scanner.close();
+    }
+}
+
