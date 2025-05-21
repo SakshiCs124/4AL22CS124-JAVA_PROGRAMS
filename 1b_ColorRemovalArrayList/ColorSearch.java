a981f7ad1353acaf44be625fa0ca573bc3079d34
SakshiCs124 <acharyasakshi399@gmail.com>
2025-05-21 14:37:44 +0530

    Add files via upload

---
+++ 1b_ColorRemovalArrayList/ColorSearch.java
@@ -0,0 +1,28 @@
+package github;
+
+import java.util.ArrayList;
+
+public class ColorSearch {
+    public static void main(String[] args) {
+        // Creating an ArrayList to store colors
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Adding different colors to the list
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        // Displaying the colors
+        System.out.println("List of colors: " + colors);
+
+        // Searching for the color "Red"
+        if (colors.contains("Red")) {
+            System.out.println("The color 'Red' is available in the list.");
+        } else {
+            System.out.println("The color 'Red' is NOT available in the list.");
+        }
+    }
+}
+
